06997b164b0ce594ed4d0c52627f92da9f6208f8
Calli Crase <callilogancrase@gmail.com>
2019-08-09 23:25:11 -0600

    small update with git init

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authRoutes = require('./routes/auth-routes');
 
 const app = express();
+var PORT = process.env.PORT || 3000;
 
 //set up to view engine 
 app.set('view engine', 'ejs');
@@ -14,6 +15,6 @@ app.get('/', (req, res)=>{
     res.render('home');
 })
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log('app is now listening for requests on port 3000')
 })
