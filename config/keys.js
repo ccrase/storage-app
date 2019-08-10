d5dead21135e22c7285b56150fc63f75412f4421
Calli Crase <callilogancrase@gmail.com>
2019-08-10 09:33:11 -0600

    added a .env file to stoer keys if gitignore doesn't work

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 //add this to the .gitignore
 module.exports = {
     google:{
-        clientID:'158368793812-ar48iggaah0kkke9rrsc33pf8nqsehs8.apps.googleusercontent.com',
-        clientSecret: 'ug0eovhT-m3EZrcQx7J1Dsa7'
+        clientID: process.env.clientID,
+        clientSecret: process.env.clientSecret
     }
 };
